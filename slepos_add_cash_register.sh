d414e64efd991bd08ce231c929aa16ad6c4fe457
Geronimo Poppino <gresco@gmail.com>
2018-03-06 15:16:43 -0300

    slepos_add_cash_register.sh: Increase version to 1.1

--- slepos_add_cash_register.sh
+++ slepos_add_cash_register.sh
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
-VERSION="1.0"
+VERSION="1.1"
 POSADMIN=/usr/sbin/posAdmin
 SUDO=/usr/bin/sudo
 [[ $(id -u) -eq 0 ]] && SUDO=""
